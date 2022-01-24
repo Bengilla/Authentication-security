c9429c2c34cb9b35eb4cd28fe1ce6a9a03757242
Bengilla <bengilla@outlook.com>
2022-01-24 16:28:31 +0800

    Add Enviroment Vars

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //jshint esversion:6
+
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -23,8 +25,7 @@ const userSchema = new mongoose.Schema ({
 });
 
 // Encrypt Something
-const secret = "Thisisbengilla"; // mongoose-encryption
-userSchema.plugin(encrypt, { secret:secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema); // 这行必须在 secret 下面
 // mongoose End-------------------------------------
